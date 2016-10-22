a0bb7852886f481b294680fbfed8b2b9af665968
CXWorks <1043806046@qq.com>
2016-10-23 00:32:54 +0800

    dfa

---
+++ lab1/gaint/tt.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+double i=1.234;
+return 0;
+}
--- lab1/lex/test.py
+++ lab1/lex/test.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
 def isChar(c):
     return (c>='a' and c<='z') or (c>='A' and c<='Z') or (c>='0' and c<='9')
-
+way=[]
 def re2nfa(re):
-    _op_={'.':6,'|':7,'(':0}
+    _op_={'.':6,'|':7,'(':10}
     #add .
     full=[]
     for i in range(len(re)):
@@ -18,10 +19,8 @@ def re2nfa(re):
     for c in full:
         if isChar(c):
             back.append(c)
-        elif c=='(':
-            while len(symb)!=0 and symb[len(symb)-1]!='(':
-                back.append(symb.pop())
-            symb.append('(')
+            if c not in way:
+                way.append(c)
         elif c==')':
             while symb[len(symb)-1]!= '(':
                 back.append(symb.pop())
@@ -32,21 +31,19 @@ def re2nfa(re):
         elif c in ['*','+','?']:
             back.append(c)
         elif c in _op_.keys():
-            while len(symb)>0 and _op_[symb[len(symb)-1]] >= _op_[c]:
+            while len(symb)>0 and symb[len(symb)-1]!='(' and _op_[symb[len(symb)-1]] >= _op_[c]:
                 back.append(symb.pop())
             symb.append(c)
-    del full,symb
     #build nfa
-    print back
     stack=[]
     for c in back:
         if isChar(c):
             g=nx.DiGraph()
-            g.add_edge(1,2,c=c)
+            g.add_edge(0,1,c=c)
             stack.append(g)
         elif c== '.':
-            g1=stack.pop()
             g2=stack.pop()
+            g1=stack.pop()
             n=len(g1)
             g=nx.disjoint_union(g1,g2)
             g.add_edge(n-1,n,e='1')
@@ -67,10 +64,96 @@ def re2nfa(re):
         elif c=='|':
             g1 = stack.pop()
             g2 = stack.pop()
-            n = len(g1)
-            g = nx.disjoint_union(g1, g2)
-            g.add_edge(n - 1, n, e='1')
-            stack.append(g)
+            n1 = len(g1)
+            n2 = len(g2)
+            s=nx.DiGraph()
+            s.add_node(0)
+            s1=nx.disjoint_union(s,g1)
+            s1.add_edge(0,1,e='1')
+            e=nx.DiGraph()
+            e.add_node(0)
+            e1=nx.disjoint_union(g2,e)
+            e1.add_edge(n2-1,n2,e='1')
+            ans=nx.disjoint_union(s1,e1)
+            ans.add_edge(0,n1+1,e='1')
+            ans.add_edge(n1,n1+n2+1,e='1')
+            stack.append(ans)
+    return g
+
+
+def findClo(g,node):
+    ans=[node]
+    #dfs
+    stack=[node]
+    while len(stack)>0:
+        n=stack.pop()
+        edge = g.edge[n]
+        for no,dic in edge.items():
+            if no not in ans and dic.has_key('e'):
+                stack.append(no)
+                ans.append(no)
+    return ans
+
+def findWay(g,ns,w):
+    ans=[]
+    for n in ns:
+        edge=g.edge[n]
+        for no,dic in edge.items():
+            if no not in ans and dic.has_key('c') and dic['c']==w:
+                #find clo
+                temp=findClo(g,no)
+                ans.extend(temp)
+    return ans
+def minDFA(node):
+    pass
+
+def nfa2dfa(nfa):
+    table={}
+    #init
+    t=findClo(nfa,0)
+    t.sort()
+    table[0]=t
+    conn={}
+    queue=deque([0])
+    while len(queue)>0:
+        n=queue.popleft()
+        n2c={}
+        n_n=table[n]
+        for c in way:
+            te=findWay(nfa,n_n,c)
+            if len(te)==0:
+                continue
+            te.sort()
+            if te not in table.values():
+                idd=len(table)
+                table[idd]=te
+                queue.append(idd)
+            else:
+                idd=table.keys()[table.values().index(te)]
+            n2c[c]=idd
+        conn[n]=n2c
+    #minimise
+    print table
+    print conn
+    s=[]
+    e=[]
+    for k,v in table.items():
+        if len(nfa.node)-1 in v:
+            e.append(k)
+        else:
+            s.append(k)
+    t2=minDFA([s,e])
+    #build graph
+    # g=nx.DiGraph()
+    # for k,v in table.items():
+    #
+    #     g.add_node(k)
+    #     if len(nfa.node) - 1 in v:
+    #         g.node[k]['e']=1
+    # for node,di in conn.items():
+    #     for c,t in di.items():
+    #         g.add_edge(node,t,c=c)
+    # return g
 
 
 
@@ -78,4 +161,18 @@ def re2nfa(re):
 
 if __name__ == '__main__':
     f=open('re')
-    re2nfa(f.readline())
+    nfa=re2nfa(f.readline())
+    g=nfa2dfa(nfa)
+
+    # print nfa.edge
+    # a=findClo(nfa,0)
+    # print a
+    # g=nx.DiGraph()
+    # g.add_edge(1,1,c='a')
+    # print g.edge
+    # nx.draw_networkx(g)
+    # plt.show()
+    # g2=nx.DiGraph()
+    # g2.add_node(1)
+    # g3=nx.disjoint_union(g2,g)
+    # print g3.edge
