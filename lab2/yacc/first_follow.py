d2726853fb5174dc3e4136d6a98ccb34a98ba087
CXWorks <1043806046@qq.com>
2016-11-14 17:40:38 +0800

    change

--- lab2/yacc/first_follow.py
+++ lab2/yacc/first_follow.py
@@ -38,27 +38,37 @@ def first_follow(rules):
 
     rules_keys = rules_dict.keys()
     key_count = len(rules_keys)
-    for k,lis in rules_dict.items():
-        if k == rules_keys[0]:
-            follow_dict[k].append('$')
-        for tmp_rule_str in lis:
+    flag=True
+    while flag:
+        print flag
+        flag=False
+        for k,lis in rules_dict.items():
+            if k == rules_keys[0]:
+                follow_dict[k].append('$')
+            for tmp_rule_str in lis:
 
-            for i in xrange(key_count):
-                if rules_keys[i] in tmp_rule_str:
-                    tmp_rule_list = tmp_rule_str
-                    current_non_term_index = tmp_rule_list.index(rules_keys[i])
+                for i in xrange(key_count):
+                    if rules_keys[i] in tmp_rule_str:
+                        tmp_rule_list = tmp_rule_str
+                        current_non_term_index = tmp_rule_list.index(rules_keys[i])
 
-                    if current_non_term_index == (len(tmp_rule_list) - 1):
-                        ext=[x for x in follow_dict[rules_keys[0]] if x not in follow_dict[rules_keys[i]]]
-                        follow_dict[rules_keys[i]].extend(ext)
-                    else:
-                        ttt=tmp_rule_list[current_non_term_index+1]
-                        if ttt not  in rules_keys and ttt not in follow_dict[rules_keys[i]]:
-                            follow_dict[rules_keys[i]].append(ttt)
+                        if current_non_term_index == (len(tmp_rule_list) - 1):
+                            ext=[x for x in follow_dict[rules_keys[0]] if x not in follow_dict[rules_keys[i]]]
+                            if len(ext)>0:
+                                flag=True
+                                follow_dict[rules_keys[i]].extend(ext)
                         else:
-                            ext=[x for x in firsts_dict[tmp_rule_list[current_non_term_index+1]] if x not in follow_dict[rules_keys[i]]]
-                            follow_dict[rules_keys[i]].extend(ext
-                                )
+                            ttt=tmp_rule_list[current_non_term_index+1]
+                            if ttt not  in rules_keys :
+                                if  ttt in follow_dict[rules_keys[i]]:
+                                    continue
+                                flag=True
+                                follow_dict[rules_keys[i]].append(ttt)
+                            else:
+                                ext=[x for x in firsts_dict[ttt] if x not in follow_dict[rules_keys[i]]]
+                                if len(ext)>0:
+                                    flag=True
+                                    follow_dict[rules_keys[i]].extend(ext)
 
     print "Follow Dict:" + " " + follow_dict.__str__()
     print "Firsts Dict:" + " " + firsts_dict.__str__()
